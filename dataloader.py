324d3ec21e1f61fa5c7e0dc5c2238a45b740e3ba
urviljivani007 <urviljivani007@gmail.com>
2022-09-24 14:53:54 +0530

    Fixing scripts to work with new python version.

--- dataloader.py
+++ dataloader.py
@@ -1,62 +1,221 @@
-'''
-https://github.com/akaraspt/deepsleepnet
-Copyright 2017 Akara Supratak and Hao Dong.  All rights reserved.
-'''
 import os
+
 import numpy as np
+
+from deepsleep.sleep_stage import print_n_samples_each_class
+from deepsleep.utils import get_balance_class_oversample
+
 import re
-class SeqDataLoader(object):
-    def __init__(self, data_dir, n_folds, fold_idx,classes):
+
+
+class NonSeqDataLoader(object):
+
+    def __init__(self, data_dir, n_folds, fold_idx):
         self.data_dir = data_dir
         self.n_folds = n_folds
         self.fold_idx = fold_idx
-        self.classes = classes
 
-    def load_npz_file(self, npz_file):
-        """Load data_2013 and labels from a npz file."""
+    def _load_npz_file(self, npz_file):
+        """Load data and labels from a npz file."""
         with np.load(npz_file) as f:
             data = f["x"]
             labels = f["y"]
             sampling_rate = f["fs"]
         return data, labels, sampling_rate
 
-    def save_to_npz_file(self, data, labels, sampling_rate, filename):
-
-        # Save
-        save_dict = {
-            "x": data,
-            "y": labels,
-            "fs": sampling_rate,
-
-        }
-        np.savez(filename, **save_dict)
     def _load_npz_list_files(self, npz_files):
-        """Load data_2013 and labels from list of npz files."""
+        """Load data and labels from list of npz files."""
         data = []
         labels = []
         fs = None
         for npz_f in npz_files:
-            print ("Loading {} ...".format(npz_f))
-            tmp_data, tmp_labels, self.sampling_rate = self.load_npz_file(npz_f)
+            print("Loading {} ...".format(npz_f))
+            tmp_data, tmp_labels, sampling_rate = self._load_npz_file(npz_f)
             if fs is None:
-                fs = self.sampling_rate
-            elif fs != self.sampling_rate:
+                fs = sampling_rate
+            elif fs != sampling_rate:
+                raise Exception("Found mismatch in sampling rate.")
+            data.append(tmp_data)
+            labels.append(tmp_labels)
+        data = np.vstack(data)
+        labels = np.hstack(labels)
+        return data, labels
+
+    def _load_cv_data(self, list_files):
+        """Load training and cross-validation sets."""
+        # Split files for training and validation sets
+        val_files = np.array_split(list_files, self.n_folds)
+        train_files = np.setdiff1d(list_files, val_files[self.fold_idx])
+
+        # Load a npz file
+        print("Load training set:")
+        data_train, label_train = self._load_npz_list_files(train_files)
+        print(" ")
+        print("Load validation set:")
+        data_val, label_val = self._load_npz_list_files(val_files[self.fold_idx])
+        print(" ")
+
+        # Reshape the data to match the input of the model - conv2d
+        data_train = np.squeeze(data_train)
+        data_val = np.squeeze(data_val)
+        data_train = data_train[:, :, np.newaxis, np.newaxis]
+        data_val = data_val[:, :, np.newaxis, np.newaxis]
+
+        # Casting
+        data_train = data_train.astype(np.float32)
+        label_train = label_train.astype(np.int32)
+        data_val = data_val.astype(np.float32)
+        label_val = label_val.astype(np.int32)
+
+        return data_train, label_train, data_val, label_val
+
+    def load_train_data(self, n_files=None):
+        # Remove non-mat files, and perform ascending sort
+        allfiles = os.listdir(self.data_dir)
+        npzfiles = []
+        for idx, f in enumerate(allfiles):
+            if ".npz" in f:
+                npzfiles.append(os.path.join(self.data_dir, f))
+        npzfiles.sort()
+
+        if n_files is not None:
+            npzfiles = npzfiles[:n_files]
+
+        subject_files = []
+        for idx, f in enumerate(allfiles):
+            if self.fold_idx < 10:
+                pattern = re.compile("[a-zA-Z0-9]*0{}[1-9]E0\.npz$".format(self.fold_idx))
+            else:
+                pattern = re.compile("[a-zA-Z0-9]*{}[1-9]E0\.npz$".format(self.fold_idx))
+            if pattern.match(f):
+                subject_files.append(os.path.join(self.data_dir, f))
+
+        if len(subject_files) == 0:
+            for idx, f in enumerate(allfiles):
+                if self.fold_idx < 10:
+                    pattern = re.compile("[a-zA-Z0-9]*0{}[1-9]J0\.npz$".format(self.fold_idx))
+                else:
+                    pattern = re.compile("[a-zA-Z0-9]*{}[1-9]J0\.npz$".format(self.fold_idx))
+                if pattern.match(f):
+                    subject_files.append(os.path.join(self.data_dir, f))
+
+        train_files = list(set(npzfiles) - set(subject_files))
+        train_files.sort()
+        subject_files.sort()
+
+        # Load training and validation sets
+        print("\n========== [Fold-{}] ==========\n".format(self.fold_idx))
+        print("Load training set:")
+        data_train, label_train = self._load_npz_list_files(npz_files=train_files)
+        print(" ")
+        print("Load validation set:")
+        data_val, label_val = self._load_npz_list_files(npz_files=subject_files)
+        print(" ")
+
+        # Reshape the data to match the input of the model - conv2d
+        data_train = np.squeeze(data_train)
+        data_val = np.squeeze(data_val)
+        data_train = data_train[:, :, np.newaxis, np.newaxis]
+        data_val = data_val[:, :, np.newaxis, np.newaxis]
+
+        # Casting
+        data_train = data_train.astype(np.float32)
+        label_train = label_train.astype(np.int32)
+        data_val = data_val.astype(np.float32)
+        label_val = label_val.astype(np.int32)
+
+        print("Training set: {}, {}".format(data_train.shape, label_train.shape))
+        print_n_samples_each_class(label_train)
+        print(" ")
+        print("Validation set: {}, {}".format(data_val.shape, label_val.shape))
+        print_n_samples_each_class(label_val)
+        print(" ")
+
+        # Use balanced-class, oversample training set
+        x_train, y_train = get_balance_class_oversample(
+            x=data_train, y=label_train
+        )
+        print("Oversampled training set: {}, {}".format(
+            x_train.shape, y_train.shape
+        ))
+        print_n_samples_each_class(y_train)
+        print(" ")
+
+        return x_train, y_train, data_val, label_val
+
+    def load_test_data(self):
+        # Remove non-mat files, and perform ascending sort
+        allfiles = os.listdir(self.data_dir)
+        npzfiles = []
+        for idx, f in enumerate(allfiles):
+            if ".npz" in f:
+                npzfiles.append(os.path.join(self.data_dir, f))
+        npzfiles.sort()
+
+        subject_files = []
+        for idx, f in enumerate(allfiles):
+            if self.fold_idx < 10:
+                pattern = re.compile("[a-zA-Z0-9]*0{}[1-9]E0\.npz$".format(self.fold_idx))
+            else:
+                pattern = re.compile("[a-zA-Z0-9]*{}[1-9]E0\.npz$".format(self.fold_idx))
+            if pattern.match(f):
+                subject_files.append(os.path.join(self.data_dir, f))
+        subject_files.sort()
+
+        print("\n========== [Fold-{}] ==========\n".format(self.fold_idx))
+
+        print("Load validation set:")
+        data_val, label_val = self._load_npz_list_files(subject_files)
+
+        # Reshape the data to match the input of the model
+        data_val = np.squeeze(data_val)
+        data_val = data_val[:, :, np.newaxis, np.newaxis]
+
+        # Casting
+        data_val = data_val.astype(np.float32)
+        label_val = label_val.astype(np.int32)
+
+        return data_val, label_val
+
+
+class SeqDataLoader(object):
+
+    def __init__(self, data_dir, n_folds, fold_idx):
+        self.data_dir = data_dir
+        self.n_folds = n_folds
+        self.fold_idx = fold_idx
+
+    def _load_npz_file(self, npz_file):
+        """Load data and labels from a npz file."""
+        with np.load(npz_file) as f:
+            data = f["x"]
+            labels = f["y"]
+            sampling_rate = f["fs"]
+        return data, labels, sampling_rate
+
+    def _load_npz_list_files(self, npz_files):
+        """Load data and labels from list of npz files."""
+        data = []
+        labels = []
+        fs = None
+        for npz_f in npz_files:
+            print("Loading {} ...".format(npz_f))
+            tmp_data, tmp_labels, sampling_rate = self._load_npz_file(npz_f)
+            if fs is None:
+                fs = sampling_rate
+            elif fs != sampling_rate:
                 raise Exception("Found mismatch in sampling rate.")
 
-            # Reshape the data_2013 to match the input of the model - conv2d
+            # Reshape the data to match the input of the model - conv2d
             tmp_data = np.squeeze(tmp_data)
-            # tmp_data = tmp_data[:, :, np.newaxis, np.newaxis]
-
-            # # Reshape the data_2013 to match the input of the model - conv1d
+            tmp_data = tmp_data[:, :, np.newaxis, np.newaxis]
+            
+            # # Reshape the data to match the input of the model - conv1d
             # tmp_data = tmp_data[:, :, np.newaxis]
 
             # Casting
             tmp_data = tmp_data.astype(np.float32)
             tmp_labels = tmp_labels.astype(np.int32)
-
-            # normalize each 30s sample such that each has zero mean and unit vairance
-            tmp_data = (tmp_data - np.expand_dims(tmp_data.mean(axis=1),axis= 1)) / np.expand_dims(tmp_data.std(axis=1),axis=1)
-
 
             data.append(tmp_data)
             labels.append(tmp_labels)
@@ -70,12 +229,12 @@ class SeqDataLoader(object):
         train_files = np.setdiff1d(list_files, val_files[self.fold_idx])
 
         # Load a npz file
-        print ("Load training set:")
+        print("Load training set:")
         data_train, label_train = self._load_npz_list_files(train_files)
-        print (" ")
-        print ("Load validation set:")
+        print(" ")
+        print("Load validation set:")
         data_val, label_val = self._load_npz_list_files(val_files[self.fold_idx])
-        print (" ")
+        print(" ")
 
         return data_train, label_train, data_val, label_val
 
@@ -92,14 +251,14 @@ class SeqDataLoader(object):
         val_files = np.array_split(npzfiles, self.n_folds)
         val_files = val_files[self.fold_idx]
 
-        print ("\n========== [Fold-{}] ==========\n".format(self.fold_idx))
+        print("\n========== [Fold-{}] ==========\n".format(self.fold_idx))
 
-        print ("Load validation set:")
+        print("Load validation set:")
         data_val, label_val = self._load_npz_list_files(val_files)
 
         return data_val, label_val
 
-    def load_data(self, seq_len = 10, shuffle = True, n_files=None):
+    def load_train_data(self, n_files=None):
         # Remove non-mat files, and perform ascending sort
         allfiles = os.listdir(self.data_dir)
         npzfiles = []
@@ -111,101 +270,46 @@ class SeqDataLoader(object):
         if n_files is not None:
             npzfiles = npzfiles[:n_files]
 
-        # subject_files = []
-        # for idx, f in enumerate(allfiles):
-        #     if self.fold_idx < 10:
-        #         pattern = re.compile("[a-zA-Z0-9]*0{}[1-9]E0\.npz$".format(self.fold_idx))
-        #     else:
-        #         pattern = re.compile("[a-zA-Z0-9]*{}[1-9]E0\.npz$".format(self.fold_idx))
-        #     if pattern.match(f):
-        #         subject_files.append(os.path.join(self.data_dir, f))
-
-        # randomize the order of the file names just for one time!
-        r_permute = np.random.permutation(len(npzfiles))
-        filename = "r_permute.npz"
-        if (os.path.isfile(filename)):
-            with np.load(filename) as f:
-                r_permute = f["inds"]
-        else:
-            save_dict = {
-                "inds": r_permute,
-
-            }
-            np.savez(filename, **save_dict)
-
-        npzfiles = np.asarray(npzfiles)[r_permute]
-        train_files = np.array_split(npzfiles, self.n_folds)
-        subject_files = train_files[self.fold_idx]
-
+        subject_files = []
+        for idx, f in enumerate(allfiles):
+            if self.fold_idx < 10:
+                pattern = re.compile("[a-zA-Z0-9]*0{}[1-9]E0\.npz$".format(self.fold_idx))
+            else:
+                pattern = re.compile("[a-zA-Z0-9]*{}[1-9]E0\.npz$".format(self.fold_idx))
+            if pattern.match(f):
+                subject_files.append(os.path.join(self.data_dir, f))
 
         train_files = list(set(npzfiles) - set(subject_files))
-        # train_files.sort()
-        # subject_files.sort()
+        train_files.sort()
+        subject_files.sort()
 
         # Load training and validation sets
-        print ("\n========== [Fold-{}] ==========\n".format(self.fold_idx))
-        print ("Load training set:")
+        print("\n========== [Fold-{}] ==========\n".format(self.fold_idx))
+        print("Load training set:")
         data_train, label_train = self._load_npz_list_files(train_files)
-        print (" ")
-        print ("Load Test set:")
-        data_test, label_test = self._load_npz_list_files(subject_files)
-        print (" ")
+        print(" ")
+        print("Load validation set:")
+        data_val, label_val = self._load_npz_list_files(subject_files)
+        print(" ")
 
-        print ("Training set: n_subjects={}".format(len(data_train)))
+        print("Training set: n_subjects={}".format(len(data_train)))
         n_train_examples = 0
         for d in data_train:
-            print d.shape
+            print(d.shape)
             n_train_examples += d.shape[0]
-        print ("Number of examples = {}".format(n_train_examples))
-        self.print_n_samples_each_class(np.hstack(label_train),self.classes)
-        print (" ")
-        print ("Test set: n_subjects = {}".format(len(data_test)))
-        n_test_examples = 0
-        for d in data_test:
-            print d.shape
-            n_test_examples += d.shape[0]
-        print ("Number of examples = {}".format(n_test_examples))
-        self.print_n_samples_each_class(np.hstack(label_test),self.classes)
-        print (" ")
+        print("Number of examples = {}".format(n_train_examples))
+        print_n_samples_each_class(np.hstack(label_train))
+        print(" ")
+        print("Validation set: n_subjects={}".format(len(data_val)))
+        n_valid_examples = 0
+        for d in data_val:
+            print(d.shape)
+            n_valid_examples += d.shape[0]
+        print("Number of examples = {}".format(n_valid_examples))
+        print_n_samples_each_class(np.hstack(label_val))
+        print(" ")
 
-        data_train = np.vstack(data_train)
-        label_train = np.hstack(label_train)
-        data_train = [data_train[i:i + seq_len] for i in range(0, len(data_train), seq_len)]
-        label_train = [label_train[i:i + seq_len] for i in range(0, len(label_train), seq_len)]
-        if data_train[-1].shape[0]!=seq_len:
-            data_train.pop()
-            label_train.pop()
-
-        data_train = np.asarray(data_train)
-        label_train = np.asarray(label_train)
-
-        data_test = np.vstack(data_test)
-        label_test = np.hstack(label_test)
-        data_test = [data_test[i:i + seq_len] for i in range(0, len(data_test), seq_len)]
-        label_test = [label_test[i:i + seq_len] for i in range(0, len(label_test), seq_len)]
-
-        if data_test[-1].shape[0]!=seq_len:
-            data_test.pop()
-            label_test.pop()
-
-        data_test = np.asarray(data_test)
-        label_test = np.asarray(label_test)
-
-        # shuffle
-        if shuffle is True:
-            # training data_2013
-            permute = np.random.permutation(len(label_train))
-            data_train = np.asarray(data_train)
-            data_train = data_train[permute]
-            label_train = label_train[permute]
-
-            # test data_2013
-            permute = np.random.permutation(len(label_test))
-            data_test = np.asarray(data_test)
-            data_test = data_test[permute]
-            label_test = label_test[permute]
-
-        return data_train, label_train, data_test, label_test
+        return data_train, label_train, data_val, label_val
 
     @staticmethod
     def load_subject_data(data_dir, subject_idx):
@@ -225,7 +329,7 @@ class SeqDataLoader(object):
             raise Exception("Invalid file pattern")
 
         def load_npz_file(npz_file):
-            """Load data_2013 and labels from a npz file."""
+            """Load data and labels from a npz file."""
             with np.load(npz_file) as f:
                 data = f["x"]
                 labels = f["y"]
@@ -233,23 +337,23 @@ class SeqDataLoader(object):
             return data, labels, sampling_rate
 
         def load_npz_list_files(npz_files):
-            """Load data_2013 and labels from list of npz files."""
+            """Load data and labels from list of npz files."""
             data = []
             labels = []
             fs = None
             for npz_f in npz_files:
-                print ("Loading {} ...".format(npz_f))
+                print("Loading {} ...".format(npz_f))
                 tmp_data, tmp_labels, sampling_rate = load_npz_file(npz_f)
                 if fs is None:
                     fs = sampling_rate
                 elif fs != sampling_rate:
                     raise Exception("Found mismatch in sampling rate.")
 
-                # Reshape the data_2013 to match the input of the model - conv2d
+                # Reshape the data to match the input of the model - conv2d
                 tmp_data = np.squeeze(tmp_data)
-                # tmp_data = tmp_data[:, :, np.newaxis, np.newaxis]
-
-                # # Reshape the data_2013 to match the input of the model - conv1d
+                tmp_data = tmp_data[:, :, np.newaxis, np.newaxis]
+                
+                # # Reshape the data to match the input of the model - conv1d
                 # tmp_data = tmp_data[:, :, np.newaxis]
 
                 # Casting
@@ -261,15 +365,7 @@ class SeqDataLoader(object):
 
             return data, labels
 
-        print ("Load data_2013 from: {}".format(subject_files))
+        print("Load data from: {}".format(subject_files))
         data, labels = load_npz_list_files(subject_files)
 
         return data, labels
-
-    @staticmethod
-    def print_n_samples_each_class(labels,classes):
-        class_dict = dict(zip(range(len(classes)),classes))
-        unique_labels = np.unique(labels)
-        for c in unique_labels:
-            n_samples = len(np.where(labels == c)[0])
-            print ("{}: {}".format(class_dict[c], n_samples))
--- prepare_physionet.py
+++ prepare_physionet.py
@@ -1,21 +1,15 @@
-'''
-https://github.com/akaraspt/deepsleepnet
-Copyright 2017 Akara Supratak and Hao Dong.  All rights reserved.
-'''
-
 import argparse
 import glob
 import math
 import ntpath
 import os
 import shutil
-import urllib
-import urllib2
+import urllib.request, urllib.parse, urllib.error
+import urllib.request, urllib.error, urllib.parse
 
 from datetime import datetime
 
 import numpy as np
-import pandas as pd
 
 from mne import Epochs, pick_types, find_events
 from mne.io import concatenate_raws, read_raw_edf
@@ -65,9 +59,9 @@ EPOCH_SEC_SIZE = 30
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--data_dir", type=str, default="data_2013/",
+    parser.add_argument("--data_dir", type=str, default="/data/physionet_sleep",
                         help="File path to the CSV or NPY file that contains walking data.")
-    parser.add_argument("--output_dir", type=str, default="data_2013/eeg_fpz_cz",
+    parser.add_argument("--output_dir", type=str, default="/data/physionet_sleep/eeg_fpz_cz",
                         help="Directory where to save outputs.")
     parser.add_argument("--select_ch", type=str, default="EEG Fpz-Cz",
                         help="File path to the trained model used to estimate walking speeds.")
@@ -94,9 +88,6 @@ def main():
     for i in range(len(psg_fnames)):
         # if not "ST7171J0-PSG.edf" in psg_fnames[i]:
         #     continue
-        # i = ii+80
-        # if i >= len(psg_fnames):
-        #     break
 
         raw = read_raw_edf(psg_fnames[i], preload=True, stim_channel=None)
         sampling_rate = raw.info['sfreq']
@@ -105,7 +96,7 @@ def main():
         raw_ch_df.set_index(np.arange(len(raw_ch_df)))
 
         # Get raw header
-        f = open(psg_fnames[i], 'r')
+        f = open(psg_fnames[i], 'r', encoding='iso-8859-1')
         reader_raw = dhedfreader.BaseEDFReader(f)
         reader_raw.read_header()
         h_raw = reader_raw.header
@@ -113,11 +104,11 @@ def main():
         raw_start_dt = datetime.strptime(h_raw['date_time'], "%Y-%m-%d %H:%M:%S")
 
         # Read annotation and its header
-        f = open(ann_fnames[i], 'r')
+        f = open(ann_fnames[i], 'r', encoding='iso-8859-1')
         reader_ann = dhedfreader.BaseEDFReader(f)
         reader_ann.read_header()
         h_ann = reader_ann.header
-        _, _, ann = zip(*reader_ann.records())
+        _, _, ann = list(zip(*reader_ann.records()))
         f.close()
         ann_start_dt = datetime.strptime(h_ann['date_time'], "%Y-%m-%d %H:%M:%S")
 
@@ -141,30 +132,31 @@ def main():
                 idx = int(onset_sec * sampling_rate) + np.arange(duration_sec * sampling_rate, dtype=np.int)
                 label_idx.append(idx)
 
-                print ("Include onset:{}, duration:{}, label:{} ({})".format(
+                print("Include onset:{}, duration:{}, label:{} ({})".format(
                     onset_sec, duration_sec, label, ann_str
                 ))
             else:
                 idx = int(onset_sec * sampling_rate) + np.arange(duration_sec * sampling_rate, dtype=np.int)
                 remove_idx.append(idx)
 
-                print ("Remove onset:{}, duration:{}, label:{} ({})".format(
-                    onset_sec, duration_sec, label, ann_str))
+                print("Remove onset:{}, duration:{}, label:{} ({})".format(
+                    onset_sec, duration_sec, label, ann_str
+                ))
         labels = np.hstack(labels)
         
-        print ("before remove unwanted: {}".format(np.arange(len(raw_ch_df)).shape))
+        print("before remove unwanted: {}".format(np.arange(len(raw_ch_df)).shape))
         if len(remove_idx) > 0:
             remove_idx = np.hstack(remove_idx)
             select_idx = np.setdiff1d(np.arange(len(raw_ch_df)), remove_idx)
         else:
             select_idx = np.arange(len(raw_ch_df))
-        print ("after remove unwanted: {}".format(select_idx.shape))
+        print("after remove unwanted: {}".format(select_idx.shape))
 
         # Select only the data with labels
-        print ("before intersect label: {}".format(select_idx.shape))
+        print("before intersect label: {}".format(select_idx.shape))
         label_idx = np.hstack(label_idx)
         select_idx = np.intersect1d(select_idx, label_idx)
-        print ("after intersect label: {}".format(select_idx.shape))
+        print("after intersect label: {}".format(select_idx.shape))
 
         # Remove extra index
         if len(label_idx) > len(select_idx):
@@ -172,12 +164,10 @@ def main():
             extra_idx = np.setdiff1d(label_idx, select_idx)
             # Trim the tail
             if np.all(extra_idx > select_idx[-1]):
-                # n_trims = len(select_idx) % int(EPOCH_SEC_SIZE * sampling_rate)
-                # n_label_trims = int(math.ceil(n_trims / (EPOCH_SEC_SIZE * sampling_rate)))
-                n_label_trims = int(math.ceil(len(extra_idx) / (EPOCH_SEC_SIZE * sampling_rate)))
-                if n_label_trims!=0:
-                    # select_idx = select_idx[:-n_trims]
-                    labels = labels[:-n_label_trims]
+                n_trims = len(select_idx) % int(EPOCH_SEC_SIZE * sampling_rate)
+                n_label_trims = int(math.ceil(n_trims / (EPOCH_SEC_SIZE * sampling_rate)))
+                select_idx = select_idx[:-n_trims]
+                labels = labels[:-n_label_trims]
             print("after remove extra labels: {}, {}".format(select_idx.shape, labels.shape))
 
         # Remove movement and unknown stages if any
@@ -202,10 +192,10 @@ def main():
         if start_idx < 0: start_idx = 0
         if end_idx >= len(y): end_idx = len(y) - 1
         select_idx = np.arange(start_idx, end_idx+1)
-        print("Data before selection: {}, {}".format(x.shape, y.shape))
+        print(("Data before selection: {}, {}".format(x.shape, y.shape)))
         x = x[select_idx]
         y = y[select_idx]
-        print("Data after selection: {}, {}".format(x.shape, y.shape))
+        print(("Data after selection: {}, {}".format(x.shape, y.shape)))
 
         # Save
         filename = ntpath.basename(psg_fnames[i]).replace("-PSG.edf", ".npz")
@@ -219,7 +209,7 @@ def main():
         }
         np.savez(os.path.join(args.output_dir, filename), **save_dict)
 
-        print ("\n=======================================\n")
+        print("\n=======================================\n")
 
 
 if __name__ == "__main__":
